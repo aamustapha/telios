012607843894a0a8e86fe6e9092bf9f7956fa101
aamustapha <abdulhakeemmustapha@gmail.com>
2021-02-23 14:13:04 +0100

    Implement input field

---
+++ components/TInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="my-3">
+    <label :for="inputId" class="text-blue-800">{{ label }}</label>
+    <input :id="inputId" v-model="val" v-bind="$attrs" class="bg-gray-200 block py-3 px-5 block w-full focus:rounded-none border-2 focus:border-gray-600"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TInput",
+  props: {
+    value: [String, Number],
+    id: String,
+    label: { type: String, required: true }
+  },
+  computed: {
+    inputId () {
+      return this.id || this.label.replace(/[\W_]+/g, "_")
+    },
+    val: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit("input", value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    loading
+    <Banner>
+      <div class=" pb-48 relative">
+        <div class="max-w-screen-sm">
+          <h2 class="text-6xl font-black ">The best hotel you will ever need</h2>
+          <summary>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo
+            consequat.
+          </summary>
+        </div>
+
+        <div class="py-8 absolute container mt-24 text-black">
+          <div class="flex justify-end">
+            <button>
+              <img src="images/arrow-right (1).svg" class="px-5 py-4 transform rotate-180 bg-white" />
+            </button>
+            <button>
+              <img src="images/arrow-right (1).svg" class="px-5 py-4 bg-blue-800"/>
+            </button>
+          </div>
+          <div class="bg-white py-8 px-12 shadow-lg flex justify-between items-end">
+            <div class="w-11/12 grid grid-cols-3 gap-4">
+              <TInput type="text" label="Check-in date" placeholder="check in date"/>
+              <TInput type="text" label="Checkout date" placeholder="check out date"/>
+              <TInput type="text" label="Rooms" placeholder="rooms"/>
+            </div>
+            <button class="bg-blue-800 h-12 w-12 mb-4">
+              <img src="images/send.svg" class="d-block mx-auto" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </Banner>
   </div>
 </template>
 
 <script>
-export default {}
+import Banner from "~/components/Banner"
+import TInput from "~/components/TInput"
+
+export default {
+  components: { TInput, Banner }
+}
 </script>
 
 <style>
