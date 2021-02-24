524f514564d3f9701d2f323869c7d7f768abc234
aamustapha <abdulhakeemmustapha@gmail.com>
2021-02-24 14:32:01 +0100

    Fix border

--- components/Reservation.vue
+++ components/Reservation.vue
@@ -3,9 +3,9 @@
     <div class=" container mx-auto py-12 text-black">
       <div class="py-8 px-12 flex justify-between ">
         <div class="grid grid-cols-4 gap-4 items-end">
-          <TInput type="text" label="Check-in date" placeholder="check in date" input-class="bg-white" />
-          <TInput type="text" label="Checkout date" placeholder="check out date" />
-          <TInput type="text" label="Rooms" placeholder="rooms" />
+          <TInput type="text" label="Check-in date" placeholder="check in date" input-class="bg-white border-gray" />
+          <TInput type="text" label="Checkout date" placeholder="check out date" input-class="bg-white border-gray" />
+          <TInput type="text" label="Rooms" placeholder="rooms" input-class="bg-white border-gray" />
           <button class="bg-blue mb-3 text-white h-12">
             Submit
             <img src="images/send.svg" class="ml-2 inline-block">
