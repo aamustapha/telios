6276cae0c78291ae4377517af511d0cb4a512114
aamustapha <abdulhakeemmustapha@gmail.com>
2021-03-21 21:37:04 +0100

    Fix reservation spacing on rooms page

--- components/Reservation.vue
+++ components/Reservation.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-gray-light my-12">
-    <div class=" container mx-auto py-12 text-black">
+    <div class="container container-fluid py-12 text-black">
       <div class="py-8 px-12 flex justify-between ">
-        <div class="grid grid-cols-4 gap-4 items-end text-medium">
+        <div class="grid grid-cols-4 gap-4 items-end w-full text-medium">
           <TInput type="text" label="Check-in date" placeholder="check in date" input-class="bg-white border-gray" />
           <TInput type="text" label="Checkout date" placeholder="check out date" input-class="bg-white border-gray" />
           <TInput type="text" label="Rooms" placeholder="rooms" input-class="bg-white border-gray" />
-          <button class="bg-blue mb-3 text-white h-12">
+          <button class="bg-blue mb-3 text-white h-12 box-content py-1 flex-grow">
             Submit
             <img src="https://ik.imagekit.io/hooli/telios/send_1v6H9jZIyghJ.svg" alt="send" class="ml-2 inline-block">
           </button>
